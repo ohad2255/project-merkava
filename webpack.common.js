49ddae1fbc1cbb7c86ff7dea57508276bc1e083a
Eyal Ella <eyal.ella@fundbox.com>
2018-01-04 09:13:06 +0200

    change buikd

--- webpack.common.js
+++ webpack.common.js
@@ -1,88 +1,85 @@
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: ["./src/index.scss", "./src/index.js"],
-    home: ["./src/home/home.scss", "./src/home/home.js"],
-    signin: ["./src/signin/signin.scss", "./src/signin/signin.js"],
-    registration: ["./src/registration/registration.scss", "./src/registration/registration.js"],
-    about: ["./src/about/about.scss", "./src/about/about.js"],
-  },
-  output: {
-    filename: "[name].min.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            { loader: "css-loader", options: { sourceMap: true } },
-            { loader: "postcss-loader", options: { sourceMap: true } },
-            { loader: "sass-loader", options: { sourceMap: true } }
-          ]
+    entry: {
+        index: ["./src/index.scss", "./src/index.js"],
+        'home/home': ["./src/home/home.scss", "./src/home/home.js"],
+        'signin/signin': ["./src/signin/signin.scss", "./src/signin/signin.js"],
+        'registration/registration': ["./src/registration/registration.scss", "./src/registration/registration.js"],
+        'about/about': ["./src/about/about.scss", "./src/about/about.js"]
+    },
+    output: {
+        filename: "[name].min.js",
+        publicPath: "/",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        { loader: "css-loader", options: { sourceMap: true } },
+                        { loader: "postcss-loader", options: { sourceMap: true } },
+                        { loader: "sass-loader", options: { sourceMap: true } }
+                    ]
+                })
+            }, {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                    options: { interpolate: "require" }
+                }
+            },
+            {
+                test: /\.(ico|jpg|jpeg|gif|webp)(\?.*)?$/,
+                loader: "file-loader",
+                options: {
+                    name: "assets/[name].[hash:8].[ext]"
+                }
+            },
+            {
+                test: /\.(otf|ttf|png|svg|woff|woff2)(\?.*)?$/,
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: "assets/[name].[hash:8].[ext]"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: "src/img", to: "img" },
+            { from: "src/fonts", to: "fonts" }
+        ]),
+        new ExtractTextPlugin("[name].css"),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html")
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['home/home'],
+            filename: path.resolve(__dirname, "dist/home/index.html"),
+            template: path.resolve(__dirname, "src/home/index.html")
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['signin/signin'],
+            filename: path.resolve(__dirname, "dist/signin/index.html"),
+            template: path.resolve(__dirname, "src/signin/index.html")
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['registration/registration'],
+            filename: path.resolve(__dirname, "dist/registration/index.html"),
+            template: path.resolve(__dirname, "src/registration/index.html")
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['about/about'],
+            filename: path.resolve(__dirname, "dist/about/index.html"),
+            template: path.resolve(__dirname, "src/about/index.html")
         })
-      }, {
-        test: /\.html$/,
-        use: {
-          loader: "html-loader",
-          options: { interpolate: "require" }
-        }
-      },
-      {
-        test: /\.(ico|jpg|jpeg|gif|webp)(\?.*)?$/,
-        loader: "file-loader",
-        options: {
-          name: "assets/[name].[hash:8].[ext]",
-        },
-
-      },
-      {
-        test: /\.(otf|ttf|png|svg|woff|woff2)(\?.*)?$/,
-        loader: "url-loader",
-        options: {
-          limit: 10000,
-          name: "assets/[name].[hash:8].[ext]",
-        },
-      },
     ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([{ from: "src/img", to: "img" }]),
-    new ExtractTextPlugin("[name].css"),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html")
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['home'],
-      filename: path.resolve(__dirname, "dist/home/index.html"),
-      template: path.resolve(__dirname, "src/home/index.html")
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['signin'],
-      filename: path.resolve(__dirname, "dist/signin/index.html"),
-      template: path.resolve(__dirname, "src/signin/index.html")
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['registration'],
-      filename: path.resolve(__dirname, "dist/registration/index.html"),
-      template: path.resolve(__dirname, "src/registration/index.html")
-    }),
-    new HtmlWebpackPlugin({
-        chunks: ['about'],
-        filename: path.resolve(__dirname, "dist/about/index.html"),
-        template: path.resolve(__dirname, "src/about/index.html")
-    }),
-
-    new UglifyJsPlugin({
-      sourceMap: true,
-      test: /\.js$/
-    })
-  ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,10 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const cssnano = require("cssnano");
 
 module.exports = merge(common, {
-  devtool: "none",
-  plugins: [
-    new CleanWebpackPlugin(["dist"]),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: cssnano,
-      cssProcessorOptions: { discardComments: { removeAll: true } }
-    })
-  ]
+    devtool: "none",
+    plugins: [
+        new CleanWebpackPlugin(["dist"])
+    ]
 });
