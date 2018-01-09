97b527bc4be024fba9063fbf0cedfbee9c5b30a5
ohad2255 <ohad2255@hotmail.com>
2018-01-09 12:28:58 +0200

    fixs V1
    all done except nav

--- src/common/common.js
+++ src/common/common.js
@@ -1,3 +1,15 @@
+new Swiper('.swiper-container', {
+
+	direction: 'horizontal',
+	loop: true,
+	speed: 1000,
+
+	pagination: {
+		el: '.main-pagination.swiper-pagination',
+		clickable: true,
+	},
+})
+
 $('.lng-btn.dropdown-toggle').dropdown()
 $('.input-toggle.dropdown-toggle').dropdown()
 $('.nav-toggle.dropdown-toggle').dropdown()
--- src/home/home.js
+++ src/home/home.js
@@ -1,12 +1,2 @@
 require('../common/common');
 
-new Swiper('.swiper-container', {
-
-	direction: 'horizontal',
-	loop: true,
-	speed: 1000,
-
-	pagination: {
-		el: '.main-pagination.swiper-pagination',
-	},
-})
