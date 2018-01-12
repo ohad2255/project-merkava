ff8e3388177dc9c7b5e74ccaa18d2a2c3225eaf9
ohad2255 <ohad2255@hotmail.com>
2018-01-12 10:57:16 +0200

    about v3

--- src/about/about.js
+++ src/about/about.js
@@ -4,9 +4,7 @@ $(function(){
   $(".Group-10").click(function(){
     $(".Group-10").toggleClass("spinEffect");
   });
-});
 
-$(function(){
   $(".Group-11b").click(function(){
     $(".Group-11b").toggleClass("spinEffect");
   });
