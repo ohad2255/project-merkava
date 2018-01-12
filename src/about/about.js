1f4db090fa8d827d87a2f40e6201c54689687e49
Eyal Ella <eyal.ella@fundbox.com>
2018-01-12 11:15:39 +0200

    fix toggels

--- src/about/about.js
+++ src/about/about.js
@@ -1,11 +1,13 @@
 require('../common/common');
 
-$(function(){
-  $(".Group-10").click(function(){
-    $(".Group-10").toggleClass("spinEffect");
-  });
+// $(function(){
+//   $(".Group-10").click(function(){
+//     $(".Group-10").toggleClass("spinEffect");
+//   });
+// });
 
-  $(".Group-11b").click(function(){
-    $(".Group-11b").toggleClass("spinEffect");
-  });
-});
+// $(function(){
+//   $(".Group-11b").click(function(){
+//     $(".Group-11b").toggleClass("spinEffect");
+//   });
+// });
