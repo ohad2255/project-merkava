f72bd8ede871e345447dc066e19002671a2a2ec5
Eyal Ella <eyal.ella@fundbox.com>
2018-02-02 18:02:49 +0200

    cards

--- src/dictionary/dictionary.js
+++ src/dictionary/dictionary.js
@@ -1,15 +1,34 @@
 var Handlebars = require('handlebars');
 require('../common/common');
 
-$(document).ready(function() {
-    var source = document.getElementById("card-template").innerHTML;
-    var template = Handlebars.compile(source);
-    var context = {
-        index: 0,
+var CARDS = {
+    a: [{
         expression: 'דוגמא',
         content: 'sdlkj sdkjhf ksjdhf kjdsh kjsdh jskh jksdhf ksdjfkjsh kjsdh ksd'
-    };
-    var html = template(context);
+    }, {
+        expression: 'אחר',
+        content: 'ssf lkjweiioweas jksdhf ksdjfkjsh kjsdh ksd'
+    }],
+    b: [{
+        expression: 'dfsdfdf',
+        content: '222222hf ksjdhf kjdsh kjsdh jskh jksdhf ksdjfkjsh kjsdh ksd'
+    }, {
+        expression: 'ddddff',
+        content: 'ssf lkjweiioweas jksdhf dskfhgfdjkhgfd'
+    }]
+};
+
+$(document).ready(function() {
+    var latter = 'a';
+    var cards = CARDS[latter];
+    var source = document.getElementById("card-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var html;
+    for (var i = 0; i < cards.length; i++) {
+        var context = CARDS[latter][i];
+        context.index = i;
+        html += template(context);
+    }
 
     $('#accordion').append(html);
     $('#accordion .collapse-button').on('click', function() {
