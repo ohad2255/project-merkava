293adcf4ed94071806565a6ba54bbc2a4474bcb9
ohad2255 <ohad2255@hotmail.com>
2018-02-26 16:29:56 +0200

    job-owners desktop and mobile ready

--- src/job-owners/job-owners.js
+++ src/job-owners/job-owners.js
@@ -1,1 +1,9 @@
 require('../common/common');
+
+$( document ).ready(function() {
+	$(".name-td").click(function() {
+		$(this).next().toggleClass("d-none");
+		$(this).find($(".grey-arrow")).toggleClass("rotate");
+	});
+});
+
