0b59af095131a2c60c37eb800b642c80ec58906a
ohad2255 <ohad2255@hotmail.com>
2018-03-11 19:51:04 +0200

    bids v4

--- src/bids/bids.js
+++ src/bids/bids.js
@@ -1,16 +1,50 @@
 require('../common/common');
 
 $(document).ready(function() {
-	    new Swiper('.swiper-container.related-documents-swiper', {
+    new Swiper('.swiper-container.related-documents-swiper', {
 	    direction: 'horizontal',
 	    loop: true,
 	    speed: 1000,
 
 	    pagination: {
-	        el: '.main-pagination.swiper-pagination',
+	        el: '.related-document-pagination.swiper-pagination',
 	        clickable: true
 	    }
 	});
+
+	new Swiper('.swiper-container.winning-bidders-swiper', {
+	    direction: 'horizontal',
+	    loop: true,
+	    speed: 1000,
+
+	    pagination: {
+	        el: '.winning-bidders-pagination.swiper-pagination',
+	        clickable: true
+	    }
+	});
+
+	new Swiper('.swiper-container.winning-bidders-swiper-2', {
+	    direction: 'horizontal',
+	    loop: true,
+	    speed: 1000,
+
+	    pagination: {
+	        el: '.winning-bidders-pagination-2.swiper-pagination',
+	        clickable: true
+	    }
+	});
+
+	// $('.related-documents-container-close').click(function() {
+	// 	$('.related-documents-container').toggleClass('d-block');
+	// 	$(this).toggleClass('border-none');
+	// 	$(this).find($(".grey-arrow")).toggleClass("rotate");
+	// });
+
+	// $('.winning-bidders-container-close').click(function() {
+	// 	$('.winning-bidders-container').toggleClass('d-block');
+	// 	$(this).toggleClass('border-none');
+	// 	$(this).find($(".grey-arrow")).toggleClass("rotate");
+	// }); 
 
  // //    if ($(window).width() > 992) {
 	// //    $('#relatedDocumentsSwiper').removeClass('related-documents-swiper');
