10dc02573b8b8307f204b594045549e5589bf0d9
ohad2255 <ohad2255@hotmail.com>
2018-03-25 19:51:58 +0300

    search-results v2

--- src/search-results/search-results.js
+++ src/search-results/search-results.js
@@ -1,1 +1,40 @@
 require('../common/common');
+
+$(document).ready(function() {
+    
+	$('.type-filter-close').click(function() {
+		$('.type-filter-wrapper').toggleClass('d-none');
+		$(this).toggleClass('border-none');
+		$(this).find($('.filter-arrow')).toggleClass('rotate'); 
+	});
+
+	$('.date-filter-close').click(function() {
+		$('.date-filter-wrapper').toggleClass('d-none');
+		$(this).toggleClass('border-none');
+		$(this).find($('.filter-arrow')).toggleClass('rotate');
+	}); 
+
+	$('.status-filter-close').click(function() {
+		$('.status-filter-wrapper').toggleClass('d-none');
+		$(this).toggleClass('border-none');
+		$(this).find($('.filter-arrow')).toggleClass('rotate');
+	});
+
+	$('.contact-topics-filter-close').click(function() {
+		$('.contact-topics-filter-wrapper').toggleClass('d-none');
+		$(this).toggleClass('border-none');
+		$(this).find($('.filter-arrow')).toggleClass('rotate');
+	});
+
+	$('.office-filter-close').click(function() {
+		$('.office-filter-wrapper').toggleClass('d-none');
+		$(this).toggleClass('border-none');
+		$(this).find($('.filter-arrow')).toggleClass('rotate');
+	});
+
+	$('.more-filter-close').click(function() {
+		$('.more-filter-wrapper').toggleClass('d-none');
+		$(this).toggleClass('border-none');
+		$(this).find($('.filter-arrow')).toggleClass('rotate');
+	});
+});
