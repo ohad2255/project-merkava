1fce795a1acefa39ac853a3c88924d58d9a6c015
ohad2255 <ohad2255@hotmail.com>
2018-04-14 16:00:05 +0300

    contact us dropzone

--- src/contact-us/contact-us.js
+++ src/contact-us/contact-us.js
@@ -1,5 +1,22 @@
+var Dropzone = require('dropzone');
 require('../common/common');
+debugger
+Dropzone.autoDiscover = false;
 
+   $(document).ready(function () {
+        $("#registration-Form").dropzone({
+            maxFiles: 2000,
+            // url: "/ajax_file_upload_handler/",
+            autoProcessQueue: false,
+            previewsContainer: ".dropzone-previews",
+            success: function (file, response) {
+                console.log(response);
+            },
+            init: function() {
+			    this.on("addedfile", function(file) { console.log("file = ", file); });
+			}
+        });
+   })
 // $(".dropzone").dropzone({
 //   url: '#'
 // })
