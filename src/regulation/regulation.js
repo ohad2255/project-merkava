9dafa629f64934a55667d2c36cebfd122054fe9d
ohad2255 <ohad2255@hotmail.com>
2018-04-18 11:40:16 +0300

    regulation mobile ready

--- src/regulation/regulation.js
+++ src/regulation/regulation.js
@@ -1,1 +1,21 @@
 require('../common/common');
+
+$(document).ready(function() {
+    new Swiper('.swiper-container.tutorials-swiper', {
+	    spaceBetween: 10,
+	    direction: 'horizontal',
+	    loop: true,
+	    speed: 1000,
+	    slidesPerView: 'auto',
+
+	    pagination: {
+	        el: '.tutorials-pagination.swiper-pagination',
+	        clickable: true
+	    }
+	});
+
+	$('.tutorials-head').on('click', function () {
+		$(".tutorials-arrow").toggleClass("rotate");
+		$(".tutorials-wrapper-mobile").toggleClass("hidden");
+	});
+});
