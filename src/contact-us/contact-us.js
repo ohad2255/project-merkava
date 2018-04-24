ebb1acc9f4a995c261286b3930ef7e35010fd276
ohad2255 <ohad2255@hotmail.com>
2018-04-24 14:17:53 +0300

    contact us ready

--- src/contact-us/contact-us.js
+++ src/contact-us/contact-us.js
@@ -1,26 +1,26 @@
 
-var Dropzone = require('dropzone');
+//var Dropzone = require('dropzone');
 require('../common/common');
-Dropzone.autoDiscover = false;
+// Dropzone.autoDiscover = false;
 
-   $(document).ready(function () {
-        $("#registration-Form").dropzone({
-            maxFiles: 2000,
-            // url: "/ajax_file_upload_handler/",
-            autoProcessQueue: false,
-            previewsContainer: ".dropzone-previews",	
-            success: function (file, response) {
-                console.log(response);
-            },
-            init: function() {
-			    this.on("addedfile", function(file) { console.log("file = ", file); });
-			},
-        });
+//    $(document).ready(function () {
+//         $("#registration-Form").dropzone({
+//             maxFiles: 2000,
+//             // url: "/ajax_file_upload_handler/",
+//             autoProcessQueue: false,
+//             previewsContainer: ".dropzone-previews",	
+//             success: function (file, response) {
+//                 console.log(response);
+//             },
+//             init: function() {
+// 			    this.on("addedfile", function(file) { console.log("file = ", file); });
+// 			},
+//         });
 
-        // $('.close-btn').on('click' , function(){
-        //     removedfile();
-        // });
-   })
+//         // $('.close-btn').on('click' , function(){
+//         //     removedfile();
+//         // });
+//    })
 // $(".dropzone").dropzone({
 //   url: '#'
 // })
