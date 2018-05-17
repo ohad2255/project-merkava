55af7c45346ccb3e8f270fae7eadc701368fa21a
ohad2255 <ohad2255@hotmail.com>
2018-05-17 14:47:15 +0300

    mailing choose mailing ready

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -2,7 +2,7 @@ require('../common');
 
 $(document).ready(function() {
 	$('.logged-in-name').on('click', function () {
-		$(this).next(".ent-dropdown-items-container").toggleClass("d-none");
+		$(this).next(".ent-dropdown-items-container").toggleClass("hidden");
 	});
 });
 
