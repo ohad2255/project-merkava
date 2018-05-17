36fa97ededba92cda719a146d9cb8f9fcd6e0ec4
ohad2255 <ohad2255@hotmail.com>
2018-05-17 14:34:23 +0300

    mailing forgot password  ready

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,3 +1,9 @@
 require('../common');
 
+$(document).ready(function() {
+	$('.logged-in-name').on('click', function () {
+		$(this).next(".ent-dropdown-items-container").toggleClass("d-none");
+	});
+});
+
 
