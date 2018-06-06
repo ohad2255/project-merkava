9727bf7ca9fe99e80fdc23375f308e0f42d90415
GLOBAL\C5269595 <gili.yaron@sap.com>
2018-06-06 12:19:04 +0300

    Added popup logic

--- src/registration/registration.js
+++ src/registration/registration.js
@@ -2,6 +2,14 @@ require('../common/common');
 
 $(document).ready(function() {
 
+    var result = window.location.hash.slice(1)
+    
+    if (result === 'fail') {
+        $('#modalFail').modal('toggle')
+    } else if (result === 'success') {
+        $('#modalSuccess').modal('toggle')
+    }
+
     var passwordInputs = [
         {
             input: '#inputPassword',
