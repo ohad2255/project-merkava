9712b732ac5e517f1522d1d48217ecda4b233590
GLOBAL\C5269595 <gili.yaron@sap.com>
2018-07-10 11:53:34 +0300

    search results infinite scroll

--- src/search-results/search-results.js
+++ src/search-results/search-results.js
@@ -2,7 +2,7 @@
 var Pikaday = require('pikaday');
 require('pikaday/scss/pikaday.scss');
 require('../common/common');
-
+require('jquery-infinite-scroll-helper');
 $(document).ready(function() {
 
 	$('.close-filter-container').click(function() {
@@ -34,5 +34,51 @@ $(document).ready(function() {
             event.preventDefault();
             return false;
         }
-    });
+	});
+	  
+	var hasMore=true;
+	var container=$('.search-results-content');
+	container.infiniteScrollHelper({
+	  loadMore: function(page,done) {
+		  if(!hasMore){
+			  return;
+		  }
+		  $.get('ajaxHtml/?s='+$.urlParam('s')+'&text='+$.urlParam('text')+'&page=' + page, function(data) {
+			  // parse json data and create new html then append
+			 	 var htmlData=$("<div>"+data+"</div>");
+				var ajaxData=htmlData.find("#ajaxData");
+				var totalResults=ajaxData.find("#totalResults").html();
+				var numberOfPages=ajaxData.find("#numberOfPages").html();
+				var currentPage=ajaxData.find("#currentPage").html();
+				var pageSize=ajaxData.find("#pageSize").html();
+
+				if (currentPage + 1 >= numberOfPages) {
+				hasMore = false;
+				}
+
+				loadResults(htmlData.find("#ajaxResults"));
+		  });
+	  },
+	  startingPageCount:0,
+	  bottomBuffer:500,
+	  doneLoading: function() {
+		  // return true if you are done doing your thing, false otherwise
+		  return false;
+	  }
+	  });
+	  function loadResults(htmlResults){
+		  container.append(htmlResults);
+	  }
+	  function buildSearchResults(result){
+  
+	  }
+	  $.urlParam = function(name){
+		  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+		  if (results==null){
+		  return null;
+		  }
+		  else{
+		  return decodeURI(results[1]) || 0;
+		  }
+	  }
 });
