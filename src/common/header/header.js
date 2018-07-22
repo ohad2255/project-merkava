195fb8af69d87a03e67085fdcc8bfa697829effa
ohad2255 <ohad2255@hotmail.com>
2018-07-22 12:18:31 +0300

    button outline fix

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -24,6 +24,15 @@ $(document).ready(function() {
              $('.main-nav-item-dropdown').removeClass('focus');
         }
     });  
+
+    function handleFirstTab(e) {
+	    if (e.keyCode === 9) { // the "I am a keyboard user" key
+	        document.body.classList.add('user-is-tabbing');
+	        window.removeEventListener('keydown', handleFirstTab);
+	    }
+	}
+
+	window.addEventListener('keydown', handleFirstTab);
 });
 
 
