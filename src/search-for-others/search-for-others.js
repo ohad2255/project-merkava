b8d74bf540191c4f80e17c8580aec22d2a1dd9ca
ohad2255 <ohad2255@hotmail.com>
2018-12-19 21:59:19 -0500

    search-for-others mobile ready

--- src/search-for-others/search-for-others.js
+++ src/search-for-others/search-for-others.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
             return false;
         }
 	});
+
+	$('.filter-mobile-wrapper').click(function() {
+		$('.options-container').toggleClass('d-block');
+		$('.grey-overlay').toggleClass('d-block');
+		//$(this).parent().toggleClass('add-border');
+		$(this).find($('.mobile-filter-arrows-wrapper')).toggleClass('rotate');
+		$('.facet').toggleClass('padding-top-facet');
+		$('.filters-list').toggleClass('padding-top-filters-list');
+		//$(".facet").css("margin-top", "90px");
+		//$('.mobile-filter-arrows-wrapper').hasClass('rotate');
+	});
 });
