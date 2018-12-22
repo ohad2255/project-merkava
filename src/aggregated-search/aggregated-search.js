e1e52fff8edce82785c5f12939d351a455620e7f
ohad2255 <ohad2255@hotmail.com>
2018-12-22 17:21:14 -0500

    aggregated-search mobile and IE ready

--- src/aggregated-search/aggregated-search.js
+++ src/aggregated-search/aggregated-search.js
@@ -2,5 +2,25 @@ require('../common/common');
 var stickybits = require('stickybits');
 
 
-stickybits($('.options-wrapper'));
+
 // stickybits($('.options-wrapper'), { stickyBitStickyOffset: 20 })
+
+$(document).ready(function() {
+	//stickybits($('.options-wrapper'));
+
+	$('.icheck').keypress(function (e) {
+        var key = e.which;
+        if (key == 13) {
+            $(this).click();
+            event.preventDefault();
+            return false;
+        }
+	});
+
+	$('.filter-mobile-wrapper').click(function() {
+		$('.options-container').toggleClass('d-block');
+		$('.grey-overlay').toggleClass('d-block');
+		$(this).find($('.mobile-filter-arrows-wrapper')).toggleClass('rotate');
+		$(".filter-mobile-container").toggleClass('border-b');
+	});
+});
