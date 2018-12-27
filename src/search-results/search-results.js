cfcef7f1818facaefa060cda86462f93da78d132
ohad2255 <ohad2255@hotmail.com>
2018-12-26 20:53:42 -0500

    search results mobile adjustment and currency input fix

--- src/search-results/search-results.js
+++ src/search-results/search-results.js
@@ -6,7 +6,17 @@ require('../common/common');
 
 $(document).ready(function() {
 
-	$('.close-filter-container').click(function() {
+	let closeFilterContainer = $('.close-filter-container');
+
+	let width = $(window).width();
+
+  	if (width < 992){
+		closeFilterContainer.next().toggleClass('d-none');
+		closeFilterContainer.toggleClass('border-none');
+		closeFilterContainer.find($('.filter-arrow')).toggleClass('rotate');
+	};
+
+	closeFilterContainer.click(function() {
 		$(this).next().toggleClass('d-none');
 		$(this).toggleClass('border-none');
 		$(this).find($('.filter-arrow')).toggleClass('rotate'); 
@@ -15,17 +25,9 @@ $(document).ready(function() {
 	$('.filter-mobile-wrapper').click(function() {
 		$('.filters-wrapper').toggleClass('d-none');
 		$('.grey-overlay').toggleClass('d-block');
-		//$(this).parent().toggleClass('add-border');
 		$(this).find($('.mobile-filter-arrows-wrapper')).toggleClass('rotate');
 		$('.facet').toggleClass('padding-top-facet');
-		//$(".facet").css("margin-top", "90px");
-		//$('.mobile-filter-arrows-wrapper').hasClass('rotate');
 	});
-
-	// $('.filter-mobile-wrapper').change(function() {
-	// 	debugger
-	// 	$(".search-results-filter-wrapper").css( "padding-top", 0);
-	// });
 
 	$('.datepicker').each(function() {
 	    $(this).data('pikaday', new Pikaday({ field: this }));
@@ -63,67 +65,43 @@ $(document).ready(function() {
 
 
 	function formatCurrency(input, blur) {
-	  // appends $ to value, validates decimal side
-	  // and puts cursor back in right position.
-	  
-	  // get input value
 	  var input_val = input.val();
+	  var isrCurrency = `&#8362;`;
 	  
-	  // don't validate empty input
 	  if (input_val === "") { return; }
 	  
-	  // original length
 	  var original_len = input_val.length;
 
-	  // initial caret position 
 	  var caret_pos = input.prop("selectionStart");
 	    
-	  // check for decimal
 	  if (input_val.indexOf(".") >= 0) {
-
-	    // get position of first decimal
-	    // this prevents multiple decimals from
-	    // being entered
 	    var decimal_pos = input_val.indexOf(".");
 
-	    // split number by decimal point
 	    var left_side = input_val.substring(0, decimal_pos);
 	    var right_side = input_val.substring(decimal_pos);
 
-	    // add commas to left side of number
 	    left_side = formatNumber(left_side);
 
-	    // validate right side
 	    right_side = formatNumber(right_side);
-	    
-	    // On blur make sure 2 numbers after decimal
+
 	    if (blur === "blur") {
 	      right_side += "00";
 	    }
 	    
-	    // Limit decimal to only 2 digits
 	    right_side = right_side.substring(0, 2);
 
-	    // join number by .
-	    input_val = left_side + '.' + right_side + '₪';
+	    input_val =+ isrCurrency + left_side + '.' + right_side;
 
 	  } else {
-	    // no decimal entered
-	    // add commas to number
-	    // remove all non-digits
 	    input_val = formatNumber(input_val);
-	    input_val = '₪' + input_val;
+	    input_val = input_val + isrCurrency;
 	    
-	    // final formatting
 	    if (blur === "blur") {
-	      input_val += ".00";
+	      input_val;
 	    }
 	  }
-	  
-	  // send updated string to input
 	  input.val(input_val);
-
-	  // put caret back in the right position
+	  
 	  var updated_len = input_val.length;
 	  caret_pos = updated_len - original_len + caret_pos;
 	  input[0].setSelectionRange(caret_pos, caret_pos);
